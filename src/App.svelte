bdbb840f3603e4e0bcd4983836687cf26f9b6981
James Wesc <jms.wsc@gmail.com>
2023-11-30 13:24:27 +1100

    added seo content

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,15 @@
     import Sketch from './Sketch.svelte'
 </script>
 
+<svelte:head>
+    <title>CS231115 by James Wesc</title>
+    <meta
+        name="description"
+        content="Code sketch for 15 Nov 2023. Taking inspiration from a UI element from a Star Wars film. Created with Svelte and SVGs."
+    />
+    <meta name="author" content="James Wesc" />
+</svelte:head>
+
 <PaneProvider>
     <Seed />
     <SvgCanvas>
